9a337c0b18c4f8208eebace40952b290b75b34e7
ren dunning <laurendunningcs@gmail.com>
2023-03-03 13:42:40 -0500

    update code for pin 12 output LED blinking

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -22,16 +22,20 @@
   https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
 */
 
+//Programmer: Lauren Dunning
+//Date: 3.3.2023
+//Program: Blink
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(12, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(12, HIGH);   // turn pin 12 to HIGH so LED turns on
   delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(12, LOW);    // turn pin 12 to LOW so LED turns off
   delay(1000);                       // wait for a second
 }
