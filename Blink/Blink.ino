413941d7f450c4e01eba44048f0dc2e91178c261
ren dunning <laurendunningcs@gmail.com>
2023-03-03 14:13:26 -0500

    added pin11 as output
    11 and 12 blinking

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -30,13 +30,16 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(12, HIGH);   // turn pin 12 to HIGH so LED turns on
+  digitalWrite(11, HIGH);   // turn pin 11 to HIGH so LED turns on
   delay(1000);                       // wait for a second
   digitalWrite(12, LOW);    // turn pin 12 to LOW so LED turns off
+  digitalWrite(11, LOW);    // turn pin 11 to LOW so LED turns off
   delay(1000);                       // wait for a second
 
   digitalWrite(12, HIGH);   // turn pin 12 to HIGH so LED turns on
