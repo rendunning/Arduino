88b6e81ab398e4a956ab8162f8ccab5a1292b476
ren dunning <laurendunningcs@gmail.com>
2023-03-07 13:59:44 -0500

    fixed previous commit

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -43,7 +43,9 @@ void loop() {
   delay(1000);                       // wait for a second
 
   digitalWrite(12, HIGH);   // turn pin 12 to HIGH so LED turns on
+  digitalWrite(11, HIGH);   // turn pin 11 to HIGH so LED turns on
   delay(2000);                       // wait for two seconds
   digitalWrite(12, LOW);    // turn pin 12 to LOW so LED turns off
-  delay(250);                       // wait for a quarter of a second
+  digitalWrite(11, LOW);    // turn pin 11 to LOW so LED turns off
+  delay(250);                       // wait for a quarter of a second                  
 }
