767a030fc1201818e11f971375e396ec28f6ab5b
ren dunning <laurendunningcs@gmail.com>
2023-03-16 14:13:56 -0400

    added resource link

--- UltrasonicSensor/UltrasonicSensor.ino
+++ UltrasonicSensor/UltrasonicSensor.ino
@@ -1,3 +1,12 @@
+//Programmer: Lauren Dunning
+//Date: 3.16.2023
+//Program: Blind Spot Monitor
+//Resource: https://bit.ly/3JKZOli
+
+/*
+ * This program will determine distances approaching
+ * vehicle that are in your blind spot and turn on a warning light
+ */
 void setup() {
   // put your setup code here, to run once:
 
