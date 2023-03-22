ceb1a2b7924e05d992309d02f2a01c686403c90a
ren dunning <laurendunningcs@gmail.com>
2023-03-22 13:59:06 -0400

    updated variables to pins

--- UltrasonicSensor/UltrasonicSensor.ino
+++ UltrasonicSensor/UltrasonicSensor.ino
@@ -8,12 +8,12 @@
  * vehicle that are in your blind spot and turn on a warning light
  */
  
-#define trigPin 2
-#define echoPin 3
-#define LEDlampRed 4
-#define  LEDlampYellow 5
-#define LEDlampGreen 6 
-#define soundbuzzer 7
+int trigPin = 7;
+int echoPin = 6;
+int LEDlampRed = 9;
+int LEDlampYellow = 10;
+int LEDlampGreen = 11;
+int soundbuzzer = 3;
 int sound  = 500;
 
 
